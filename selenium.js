97d2217b672992849ca33ee3c1c69a9ed5225e6a
prasanna1211 <prasanna@codebrahma.com>
2018-09-04 14:41:52 +0530

    add selenium test cases and circle-ci config

---
+++ selenium.js
@@ -0,0 +1,14 @@
+var webdriverio = require('webdriverio');
+
+var options = {
+    desiredCapabilities: {
+        browserName: 'chrome'
+    }
+};
+
+webdriverio
+  .remote(options)
+  .init()
+  .url('http://localhost:3000/')
+  .saveScreenshot('buddyworks.png')
+  .end();
