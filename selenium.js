0f24abbbc79465f1161e51efecf8c1fb862ba783
prasanna1211 <prasanna@codebrahma.com>
2018-09-05 12:27:02 +0530

    update config

--- selenium.js
+++ selenium.js
@@ -1,17 +1,3 @@
-// const webdriver = require('selenium-webdriver');
-// const By = webdriver.By;
-//
-// let driver = new webdriver.Builder().forBrowser('chrome').build();
-//
-// driver.get('http://localhost:3000/');
-//
-// driver.wait(function() {
-//     return driver.isElementPresent(By.id('input-box'));
-// }, 3000, '\nFailed to load login page.');
-//
-// driver.findElement(By.id('input-box')).click();
-
-
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
 (async function example() {
@@ -20,6 +6,6 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
     await driver.get('http://localhost:3000/');
     await driver.findElement(By.id('input-box')).click();
   } finally {
-    // await driver.quit();
+    await driver.quit();
   }
 })();
