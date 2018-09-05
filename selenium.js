e326b63449a68e923207271a4edd9e2bc6d2db72
prasanna1211 <prasanna@codebrahma.com>
2018-09-05 12:34:58 +0530

    update config

--- selenium.js
+++ selenium.js
@@ -1,11 +1,30 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const webdriver = require('selenium-webdriver');
+const {until} = require('selenium-webdriver');
+const By = webdriver.By;
 
-(async function example() {
-  let driver = await new Builder().forBrowser('chrome').build();
-  try {
-    await driver.get('http://localhost:3000/');
-    await driver.findElement(By.id('input-box')).click();
-  } finally {
-    await driver.quit();
-  }
-})();
+let driver = new webdriver.Builder().forBrowser('chrome').build();
+
+driver.get('http://localhost:3000/');
+//
+// driver.wait(function() {
+//     return driver.isElementPresent(By.id('input-box'));
+// }, 3000, '\nFailed to load login page.');
+
+driver.wait(until.elementLocated(By.id('input-box')), 10 * 1000).then(el => {
+    driver.findElement(By.id('input-box')).click();
+});
+
+
+
+
+// const {Builder, By, Key, until} = require('selenium-webdriver');
+//
+// (async function example() {
+//   let driver = await new Builder().forBrowser('chrome').build();
+//   try {
+//     await driver.get('http://localhost:3000/');
+//     await driver.findElement(By.id('input-box')).click();
+//   } finally {
+//     await driver.quit();
+//   }
+// })();
